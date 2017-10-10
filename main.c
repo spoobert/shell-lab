ea255f6bdf13d32767a4500022e45f8a3abedcc3
devin t brown <toel3my@gmail.com>
2017-10-10 12:13:36 -0700

    changed do pipe warning to accuretly reflect where they occured changed name of 2cargv to rOperand to reflect its relationship to the pipe command

--- main.c
+++ main.c
@@ -15,21 +15,21 @@ int doPipe( int pipeIndex, char *argv[] ){
     int pfd[2];
     pid_t pid;
     argv[pipeIndex] = NULL;
-    char **c2argv = &argv[ pipeIndex + 1];
+    char **rOperand = &argv[ pipeIndex + 1];
     
     if ( pipe(pfd) == -1 )
-        fatalError( "Nope on that pipe son" );
+        fatalError( "pipe setup failed" );
  
     switch ( pid = fork() ){
         case -1:
-            fatalError( " doing second fork ");
+            fatalError( "stdin fork");
         case 0:
             if ( dup2( pfd[0], STDIN_FILENO ) == -1 )
-                fatalError( " attaching pipe as stdin in child failed " );
+                fatalError( "failed to dup pfd[0]" );
             if( close( pfd[0] ) == -1 || close( pfd[1] ) == -1 )
-                fatalError( "problems closing child 2 pfd's" );
-            execvp( c2argv[0], c2argv );
-            fatalError( "c2argv execvp failed" );
+                fatalError( "problems closing pfd[0] || prd[1]" );
+            execvp( rOperand[0], rOperand );
+            fatalError( "rOperand execvp failed" );
 
     }
 
@@ -60,10 +60,10 @@ int doCommand( int argc, char *argv[]){
     }
     switch ( pid = fork() ){
         case -1:
-            fatalError( "First fork failed" );
+            fatalError( "doCommand fork failed" );
         case 0:
             execvp( argv[0], argv );
-            fatalError( "could not doCommand" );
+            fatalError( "execvp doCommand failed" );
     }
     while( wait(NULL) != -1 );
 
