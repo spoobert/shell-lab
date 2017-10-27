6d108bbc87c17f1b5edc8f835741d624c4325667
devin t brown <toel3my@gmail.com>
2017-10-27 01:29:53 -0700

    case for pipe setup

--- main.c
+++ main.c
@@ -44,21 +44,26 @@ void    doInsertion( int insertIndex, char *argv[] ){
 // do some changes
 
 //who outputs the no such file or directory
+
+//TODO pass doPipe num words for argument parsing
+//TODO use return value from pipe to indicate where 
+//we are in the parsing
 int doPipe( int pipeIndex, char *argv[]){
-    int pfd[2];
+    int pfd[4];
     pid_t pid;
     argv[pipeIndex] = NULL;
     char **rOperand = &argv[ pipeIndex + 1 ];
     
     if ( pipe(pfd) == -1 )
-        fatalError( "pipe setup failed" );
-
-  
+        fatalError( "first pipe setup failed" );
+    if( pipe( pfd + 2) == -1 )
+        fatalError("second pipe problem");
 
     switch ( pid = fork() ){
         case -1:
             fatalError( "stdin fork");
         case 0:
+        //what does the integer in pfd mean
             if ( dup2( pfd[0], STDIN_FILENO ) == -1 )
                 fatalError( "failed to dup pfd[0]" );
             if( close( pfd[0] ) == -1 || close( pfd[1] ) == -1 )
@@ -86,7 +91,67 @@ int doPipe( int pipeIndex, char *argv[]){
     while( wait(NULL) != -1 );
     return 1;
 }
+
+int pipeHandler( int argc, char *argv[] ){
+    char **cmds[argc];
+    enum { PIPE0IN , PIPE0OUT , PIPE1IN , PIPE1OUT };
+    int j = 0;
+    cmds[j++] = &argv[0];
+    for( int i = 0 ; i < argc ; i ++ ){
+        if( *argv[i] == '|' ){
+            argv[i] = NULL;
+            cmds[j++] = &argv[ i + 1 ];
+        
+        }
+    }
+
+    //for( int i = 0; i < j ; i++){
+    //    printf( "%s\n" , *cmds[i] );
+    //}
+
+    int pipes[4];
+    pipe(pipes);
+    pipe(pipes + 2);
+    int k = 0;
+    for( int job = 0 ; job < j ; job++ ){
+        if( fork() == 0 ){
+            //NeeDs to be gneralized
+            switch(job) {
+                case 0 :
+                    dup2( pipes[PIPE0OUT] , STDOUT_FILENO );
+                    break;
+                case 1 : 
+                    dup2( pipes[PIPE0IN] , STDIN_FILENO );
+                    dup2( pipes[PIPE1OUT], STDOUT_FILENO );
+                    break;
+                case 2 : 
+                    dup2( pipes[PIPE1IN], STDIN_FILENO );
+                    break;
+                default : fatalError( "KILLER BUNNY AHEAD" );
+            
+            }
+
+            //TODO  instead of 4 need number of pipes 
+            for( int i = 0 ; i < 4 ; i++ )
+                close( pipes[i] );
+            
+            execvp( *cmds[job] , cmds[job] );
+            
+    
+        }
+    }
+    //[ ls | grep test.c | wc ]
+    // pipeIndex[ 1 , 4 ]
+    for( int i = 0 ; i < 3 ; i ++){
+         wait(NULL); 
+    }
+    return 0;
+        
+}
+
+
 //you may want to have wait
+#if 0
 int doCommand( int argc, char *argv[]){
     pid_t pid;
     for( int i = 0 ; i < argc ; i++ ){
@@ -112,6 +177,7 @@ int doCommand( int argc, char *argv[]){
     return 1; 
 
 }
+#endif
 
 int main() {
     // Buffer for reading one line of input
@@ -122,8 +188,8 @@ int main() {
     // or some other input error occurs
     while( fgets(line, MAX_LINE_CHARS, stdin) ) {
         int num_words = split_cmd_line(line, line_words);
-        
-        doCommand( num_words, line_words );
+        printf( "None Shall PASS, w/o cmmd " );
+        pipeHandler( num_words, line_words );
         
     }
 
